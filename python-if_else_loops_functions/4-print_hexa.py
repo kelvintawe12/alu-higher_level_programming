7f161eafee6a29afc7430d4ff2aa39c1ac62dfc0
kelvintawe12 <k.tawe@alustudent.com>
2024-06-26 15:42:20 -0700

    printing integers

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# printing integers and using the string format
+# Print numbers from 0 to 98 in decimal and hexadecimal
+for i in range(99):
+    print("{} = 0x{:x}".format(i, i))
