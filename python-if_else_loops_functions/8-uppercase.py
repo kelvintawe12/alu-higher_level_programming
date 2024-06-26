37086e603c32a6943675b5d225becb47300ee08f
kelvintawe12 <k.tawe@alustudent.com>
2024-06-26 16:03:42 -0700

    file 8 check

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,12 +5,11 @@ def uppercase(s):
     Prints the input string in uppercase followed by a new line.
     """
     for char in s:
-        # Convert lowercase characters to uppercase using ASCII values
         if 'a' <= char <= 'z':
             print("{}".format(chr(ord(char) - 32)), end='')
         else:
             print("{}".format(char), end='')
-    print()  # Print a newline
+    print()
 
 # Example usage
 uppercase("best")  # Output: BEST
