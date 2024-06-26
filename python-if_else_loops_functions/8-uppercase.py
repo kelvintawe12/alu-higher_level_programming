c36ef0b180c62876d7806da85d3509c0d9674659
kelvintawe12 <k.tawe@alustudent.com>
2024-06-26 16:20:56 -0700

    complex task

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
 
 def uppercase(s):
-    """
-    Prints the input string in uppercase followed by a new line.
-    """
-    for char in s:
-        if 'a' <= char <= 'z':
-            print("{}".format(chr(ord(char) - 32)), end='')
-        else:
-            print("{}".format(char), end='')
-    print()
-
-# Example usage
-uppercase("best")  # Output: BEST
-uppercase("Best School 98 Battery street")
+    print("".join(chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in s))
