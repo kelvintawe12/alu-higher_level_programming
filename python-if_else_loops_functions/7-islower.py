df6e1fdc8a3e6a9972100e84b72f2ad6252d5430
kelvintawe12 <k.tawe@alustudent.com>
2024-06-27 09:12:54 -0700

    my work

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ard(c) <= 122:
+        return True
+    else:
+        return False
 
-def get_unicode_code_point(c):
-    """
-    Returns the Unicode code point of the given character.
-    """
-    return ord(c)
-
-# Example usage:
-print(get_unicode_code_point('a'))  
-print(get_unicode_code_point('\u2020'))
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-
-def uppercase(s):
-    print("".join(chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in s))
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end=""
+     print()           
