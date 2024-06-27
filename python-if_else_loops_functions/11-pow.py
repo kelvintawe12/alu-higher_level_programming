8aca9a90c08df15d398b9f73ed47dc4198a10dda
kelvintawe12 <k.tawe@alustudent.com>
2024-06-27 11:09:30 -0700

    a**b

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-
 def pow(a, b):
-    """
-    Returns the value of a^b.
-    """
-    result = 1
-    for _ in range(abs(b)):
-        result *= a
-    if b < 0:
-        return 1 / result
-    return result
-
-# Example usage:
-print(pow(2, 2)
-print(pow(98, 2))
-print(pow(98, 0))   
-print(pow(100, -2)) 
-print(pow(-4, 5))
+    return (a ** b)
