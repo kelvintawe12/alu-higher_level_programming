4d3b493b67e1b866d4fec65f0521e7a06b6e3460
kelvintawe12 <k.tawe@alustudent.com>
2024-06-27 09:28:04 -0700

    done well

--- python-if_else_loops_functions/7-main.py
+++ python-if_else_loops_functions/7-main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-islower = __import__('7-islower').islower
+def custom_islower(char):
+    return char.islower()
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+print("a is {}".format("lower" if custom_islower("a") else "upper"))
+print("H is {}".format("lower" if custom_islower("H") else "upper"))
+print("A is {}".format("lower" if custom_islower("A") else "upper"))
+print("3 is {}".format("lower" if custom_islower("3") else "upper"))
+print("g is {}".format("lower" if custom_islower("g") else "upper"))
+
