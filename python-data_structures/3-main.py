ae00043ea4b90e33226c12d9bfa6251a7af67b35
kelvintawe12 <k.tawe@alustudent.com>
2024-06-27 21:03:31 -0700

    submiting the file

--- python-data_structures/3-main.py
+++ python-data_structures/3-main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+def print_reversed_list_integer(my_list):
+    for item in reversed(my_list):
+        print(item)
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
