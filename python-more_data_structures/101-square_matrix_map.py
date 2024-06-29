441825045749ce222c18e87a3ea245a7f596c356
kelvintawe12 <k.tawe@alustudent.com>
2024-06-29 07:24:41 -0700

    last commit

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    square_matrix_map = lambda m: [list(map(lambda x: x ** 2, row)) for row in m]
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
