ecf9b0812e25576c6a7f90d69b764f8140b0d64b
kelvintawe12 <k.tawe@alustudent.com>
2024-07-04 15:33:40 -0700

    strength k.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    num = 0
     try:
-        for i in range(x):
-            print(my_list[i], end=" ")
-            count += 1
+        for item in range(x):
+            print("{}".format(my_list[i], end=" "))
+            num += 1
     except IndexError:
         pass
-    finally:
         print()
-        return count
+        return num
