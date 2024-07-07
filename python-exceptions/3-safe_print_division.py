27d3b01ad6586d2f69e03e033b00b28fcfb78756
kelvintawe12 <k.tawe@alustudent.com>
2024-07-07 14:19:57 -0700

    done well

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -8,14 +8,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result:", result)
         return result
-
-# Example usage:
-a = 10
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 10
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
