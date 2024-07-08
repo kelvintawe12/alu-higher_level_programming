61515eb3a1e9c2adcb462a2cfcf051061507b5f1
kelvintawe12 <k.tawe@alustudent.com>
2024-07-08 04:12:40 -0700

    my work

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,27 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-    except (ValueError, TypeError):
-        pass
+    printed_count = 0
+    try:
+        while count < x:
+            if isinstance(my_list[count], int):
+                print("{:d}".format(my_list[count]), end="")
+                printed_count += 1
+            count += 1
+    except IndexError:
+        raise
     print()
-    return count
+    return printed_count
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, 7)
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list_integers(my_list, 9)
+    print("nb_print: {:d}".format(nb_print))
