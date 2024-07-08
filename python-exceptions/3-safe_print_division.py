4b9cae46e0a38d58d48c2e14335343d683d0ae3b
kelvintawe12 <k.tawe@alustudent.com>
2024-07-08 05:27:39 -0700

    done well with course

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,10 +6,16 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result:", result)
-        return result
-a = 10
-b = 2
-result = safe_print_division(a, b)
-if result is not None:
-    print(f"10 / 2 = {result:.1f}")
+        print("Inside result: {}".format(result))
+    return result
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
