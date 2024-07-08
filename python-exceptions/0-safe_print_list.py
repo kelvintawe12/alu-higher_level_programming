cbafb1ac699c79146aa674df8deccc8c8973e2d4
kelvintawe12 <k.tawe@alustudent.com>
2024-07-08 03:45:46 -0700

    done with course work

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,23 +1,24 @@
-#!/usr/bin/pyhon3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        count = 0
         for i in range(x):
             print(my_list[i], end="")
             count += 1
     except IndexError:
         pass
-    finally:
-        print()
-        return count
-my_list = [1, 2, 3, 4, 5]
+    print()  # To ensure a new line is printed after the loop
+    return count
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
 
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
