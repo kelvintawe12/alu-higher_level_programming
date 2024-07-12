1a150629d4ee6fd8d19d8592ca1279c6c5156b4e
kelvintawe12 <k.tawe@alustudent.com>
2024-07-11 19:54:43 -0700

    fine

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""this module will be used to execute a ,ajority of code,"""
+
+
 class LockedClass:
+    """this is a simple class for the code which is to displlay thr str"""
     def __setattr__(self, attr, value):
         if not hasattr(self, 'first_name') and attr != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
