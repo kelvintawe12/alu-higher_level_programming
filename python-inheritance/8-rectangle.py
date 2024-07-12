47515cebf79bbaeb187f408f0ec0aab971a6fed7
kelvintawe12 <k.tawe@alustudent.com>
2024-07-11 18:01:32 -0700

    first file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@ This module defines a class Rectangle that inherits from BaseGeometry.
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     A class representing a rectangle inheriting from BaseGeometry.
