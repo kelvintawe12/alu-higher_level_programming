e8db7dda6eb87038337805a4f0b0ecda98057bb0
kelvintawe12 <k.tawe@alustudent.com>
2024-07-11 18:12:18 -0700

    finely done

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" this creates a class and is specific to the objectof dthe class."""
+
 def is_same_class(obj, a_class):
     """
     Returns True if the object is exactly an instance of the specified class;
