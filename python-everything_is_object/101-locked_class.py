57c798dd83223a4ae5143e08bc5847522560f61f
kelvintawe12 <k.tawe@alustudent.com>
2024-07-11 19:59:04 -0700

    hard work

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -8,3 +8,11 @@ class LockedClass:
         if not hasattr(self, 'first_name') and attr != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
         super().__setattr__(attr, value)
+
+if __name__ == "__main__":
+    lc = LockedClass()
+    lc.first_name = "John"
+    try:
+        lc.last_name = "Snow"
+    except AttributeError as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
