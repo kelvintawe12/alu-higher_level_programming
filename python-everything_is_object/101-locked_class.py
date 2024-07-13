01ce3ed6bf49120ed1147ad75244ddb4e050254d
kelvintawe12 <k.tawe@alustudent.com>
2024-07-13 12:35:09 -0700

    done with advanced tasks

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""this module will be used to execute a ,ajority of code,"""
+
+'''creation of a class'''
 
 
 class LockedClass:
-    """this is a simple class for the code which is to displlay thr str"""
-    def __setattr__(self, attr, value):
-        if not hasattr(self, 'first_name') and attr != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
-        super().__setattr__(attr, value)
-
-if __name__ == "__main__":
-    lc = LockedClass()
-    lc.first_name = "John"
-    try:
-        lc.last_name = "Snow"
-    except AttributeError as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    '''Prevents dealing with any kind of attributes
+    except "first_name"
+    '''
+    __slots__ = ["first_name"]
