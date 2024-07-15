412fc915c05aa3dc3b6b29b5074cf91706c6920b
kelvintawe12 <k.tawe@alustudent.com>
2024-07-15 16:47:33 -0700

    sql manipulation.

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,4 @@
-Enter password: 
-Database                                                                                     
-hbtn_0c_0                                                                                    
-information_schema                                                                           
-mysql                                                                                        
-performance_schema                                                                           
-sys
+-- This script lists all databases of the MySQL server
+-- Usage: mysql -hlocalhost -uroot -p < 0-list_databases.sql
+
+SHOW DATABASES;
