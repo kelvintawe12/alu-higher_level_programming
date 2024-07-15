a315008692de6ae8b6a873ca99f76e72535903c3
kelvintawe12 <k.tawe@alustudent.com>
2024-07-15 13:11:30 -0700

    done well

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module: 0-read_file
+
+This module provides a function to read a text file and print its contents to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        read_file(sys.argv[1])
+    else:
+        print("Usage: ./0-main.py <filename>")
