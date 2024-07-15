15db3fca9bc38d6f57e40a6ebf37f7e43f858f3f
kelvintawe12 <k.tawe@alustudent.com>
2024-07-15 14:50:59 -0700

    pascals triangle

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the Student class with public instance attributes
-and methods for JSON serialization.
+This module defines the Student class with public instance attributes.
 """
 
 
@@ -9,7 +8,8 @@ class Student:
     """Defines a student by first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the student with the provided first name, last name,
+        """
+        Initializes the student with the provided first name, last name,
         and age.
         """
         self.first_name = first_name
@@ -17,11 +17,12 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
-        
+        """
+        Retrieves a dictionary representation of a Student instance.
+
         If attrs is a list of strings, only those attributes are retrieved.
         Otherwise, all attributes are retrieved.
         """
         if attrs is None:
             return self.__dict__
-        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, 
+        return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
