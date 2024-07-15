10689964fef5599d138213847d9aa54275b54b6f
kelvintawe12 <k.tawe@alustudent.com>
2024-07-15 13:23:52 -0700

    read file.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,20 +2,21 @@
 """
 Module: 0-read_file
 
-This module provides a function to read a text file and print its contents to stdout.
+This module provides a function to read a text file and print its
+contents to stdout.
 """
-
 
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+                        Defaults to an empty string.
     """
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
-        print(content)
+        print(content, end="")
 
 
 if __name__ == "__main__":
