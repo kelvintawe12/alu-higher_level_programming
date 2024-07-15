77b9a29efc85fe867b23bb7191f8cc3705009c87
kelvintawe12 <k.tawe@alustudent.com>
2024-07-15 16:51:08 -0700

    fine sql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- This script creates the database hbtn_0c_0 if it does not already exist
+-- Usage: mysql -hlocalhost -uroot -p < 1-create_database_if_missing.sql
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
