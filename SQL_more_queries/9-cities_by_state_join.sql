dd3ab287fd4275d4c11209be49a225bc34f29a2d
kelvintawe12 <k.tawe@alustudent.com>
2024-07-21 08:14:40 -0700

    genre ID by show

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- This query lists all cities along with their corresponding state names
 -- from the database hbtn_0d_usa, sorted in ascending order by cities.id
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
