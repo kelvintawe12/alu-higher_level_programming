59fe4fdc33fe1e54141fff6a93babb1496bc52c9
kelvintawe12 <k.tawe@alustudent.com>
2024-07-21 11:26:55 -0700

    finely done

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,18 +1,5 @@
--- Ensure the database hbtn_test_db_4 exists
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
-
--- Use the specified database
-USE hbtn_test_db_4;
-
--- Create the table id_not_null if it does not exist
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    name VARCHAR(256)
+-- CREATES A TABLE id_not_null
+CREATE TABLE IF NOT EXISTS `id_not_null`(
+	`id` INT DEFAULT 1,
+	`name` VARCHAR(256)
 );
-
--- Insert the expected data
-INSERT INTO id_not_null (id, name) VALUES (1, "Holberton School");
-INSERT INTO id_not_null (id, name) VALUES (1, "Python is cool");
-INSERT INTO id_not_null (id, name) VALUES (2, "Holberton");
-INSERT INTO id_not_null (id, name) VALUES (3, "School");
-INSERT INTO id_not_null (id, name) VALUES (4, "C is fun");
--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,19 +1,5 @@
--- Ensure the database hbtn_0d_2 exists
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Use the specified database
-USE hbtn_0d_2;
-
--- Create the table unique_id if it does not exist
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT UNIQUE DEFAULT 1,
-    name VARCHAR(256)
+-- CREATES A TABLE unique_id
+CREATE TABLE IF NOT EXISTS `unique_id`(
+	`id` INT DEFAULT 1 UNIQUE,
+	`name` VARCHAR(256)
 );
-
--- Insert data into the table
-INSERT IGNORE INTO unique_id (id, name) VALUES 
-(1, 'Holberton School'),
-(2, 'Holberton'),
-(3, 'School'),
-(4, 'C is fun'),
-(5, 'Python is cool');
