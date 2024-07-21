15c2ae785fbcd4fb910cd0368f22b18d3c9276a0
kelvintawe12 <k.tawe@alustudent.com>
2024-07-21 09:46:16 -0700

    fine

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -9,3 +9,11 @@ CREATE TABLE IF NOT EXISTS unique_id (
     id INT UNIQUE DEFAULT 1,
     name VARCHAR(256)
 );
+
+-- Insert data into the table
+INSERT IGNORE INTO unique_id (id, name) VALUES 
+(1, 'Holberton School'),
+(2, 'Holberton'),
+(3, 'School'),
+(4, 'C is fun'),
+(5, 'Python is cool');
