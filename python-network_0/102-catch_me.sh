18c08e44800e86ece3a7123a3d85a0de7dcdd904
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 16:27:17 +0000

    you got me

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Makes a request and print a specific message catch me if you can
-curl -sX POST -H "X-Special-Header: CatchMe" 0.0.0.0:5000/catch_me
+# Sends a series of requests to the specified URL to trigger the response "You got me!"
+curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me -o /dev/null
+curl -sL 0.0.0.0:5000/catch_me | grep "You got me!"
