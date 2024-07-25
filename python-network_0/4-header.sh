23bf280f093bd336a50bf3e6ac6fc634908991ec
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 17:08:17 +0000

    holberton school!

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send GET request with custom header and check response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1" | grep -q "Hello Holberton School!" && echo "OK" || echo "FAIL"
+#send updates the header
+curl -sG "$1" -H "X-School-User-Id: 98"
