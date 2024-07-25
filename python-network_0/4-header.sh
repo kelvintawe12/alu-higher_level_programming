873a88a3cd8ee97f2e13433c43395eec1d49b41d
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 16:53:35 +0000

    okay;

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send GET request with custom header and display the response body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Send GET request with custom header and check response
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1" | grep -q "Hello Holberton School!" && echo "OK" || echo "FAIL"
