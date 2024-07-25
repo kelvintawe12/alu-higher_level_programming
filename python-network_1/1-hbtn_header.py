56844cc9b06a2778ef9b9d1d6c10da7159a7a245
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 17:51:17 +0000

    fine

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script takes in a URL, sends a request to the URL, and displays the value of
+This script takes in a URL, sends a request to the URL, and displays the.
 the X-Request-Id variable found in the header of the response.
 """
 
