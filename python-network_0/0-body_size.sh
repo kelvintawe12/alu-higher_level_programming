787a1e39dcf8e3114dad9b750e1bcab214b23526
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 14:19:32 +0200

    body file

---
+++ python-network_0/0-body_size.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Usage: ./0-body_size.sh <URL>
+# Example: ./0-body_size.sh http://0.0.0.0:5000
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+URL=$1
+
+# Send a request to the URL and store the size of the response body in bytes
+body_size=$(curl -s -w '%{size_download}' -o /dev/null "$URL")
+
+echo $body_size
