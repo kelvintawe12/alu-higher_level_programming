5d189fd0296d08984207da080c43d997fb26ea48
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 16:47:54 +0000

    ok

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a PUT request to the specified URL with a custom header to trigger the expected response
-curl -s -X PUT -H "X-Secret-Header: MySecretValue" 0.0.0.0:5000/catch_me
+# Send GET request with custom header and display the response body
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
