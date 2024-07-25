deafd3ad2fe8f50fcfa9a935778081111a20aac3
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 16:37:10 +0000

    complex task

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a series of requests to the specified URL to trigger the response "You got me!"
-curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me -o /dev/null
-curl -sL 0.0.0.0:5000/catch_me | grep "You got me!"
+# This script sends a request to 0.0.0.0:5000/catch_me and expects to receive "You got me!" in the response.
+curl -sL -X PUT -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
