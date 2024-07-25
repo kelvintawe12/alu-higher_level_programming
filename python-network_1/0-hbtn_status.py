01bbad59ed78c1baa156699e5152c93c5c5af8b2
kelvintawe12 <k.tawe@alustudent.com>
2024-07-25 18:31:25 +0000

    finely done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """
-This script fetches the status of a given URL and displays details about the.
+This script fetches the status of a given URL and displays details about the response.
+
+The details include:
+- Type of the response content
+- Raw content
+- UTF-8 decoded content
 """
 
 import urllib.request
@@ -8,18 +13,24 @@ import urllib.request
 
 class URLFetcher:
     def __init__(self, url):
+        """
+        Initializes URLFetcher with the URL to fetch.
+        """
         self.url = url
 
     def fetch(self):
+        """
+        Fetches the content of the URL and displays the type, content, and UTF-8 content.
+        """
         with urllib.request.urlopen(self.url) as response:
             body = response.read()
             print("Body response:")
             print("\t- type:", type(body))
-            print(f"\t- content: {body}")
-            print(f"\t- utf8 content: {body.decode('utf-8')}")
-
+            print("\t- content:", body)
+            print("\t- utf8 content:", body.decode('utf-8'))
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'  # Update the URL here as needed
     fetcher = URLFetcher(url)
     fetcher.fetch()
+
