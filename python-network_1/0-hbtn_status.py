cf005880739e0080542ce4365c21bb326225047b
kelvintawe12 <k.tawe@alustudent.com>
2024-07-26 21:21:05 +0000

    finely done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,17 +4,17 @@ Fetches and displays the status of a URL.
 """
 
 import urllib.request
-
+#imports the requests.
 def fetch_status():
-#Fetches the content of a URL and prints its type, content, and UTF-8 content.
+#Fetches the content of a URL and prints its type, cont.
     url = 'https://alu-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
-
+#opens the url.
     print("Body response:")
     print(f"    - type: {type(body)}")
     print(f"    - content: {body}")
     print(f"    - utf8 content: {body.decode('utf-8')}")
-
+#the print .
 if __name__ == "__main__":
     fetch_status()
