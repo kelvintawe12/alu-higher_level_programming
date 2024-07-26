a1a220b75f16ba1d3c123882317a78a987712208
kelvintawe12 <k.tawe@alustudent.com>
2024-07-26 22:49:23 +0000

    hard things

--- python-test_driven_development/add_integer.py
+++ python-test_driven_development/add_integer.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
 Module for adding two integers.
-
 This module provides a function to add two numbers,
 ensuring that both inputs are integers or floats,
 and handles type errors appropriately.
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats after casting them to integers.
-
     Parameters:
     a (int, float): The first number.
     b (int, float, optional): The second number, defaults to 98.
-
     Returns:
     int: The sum of a and b, cast to an integer.
 
