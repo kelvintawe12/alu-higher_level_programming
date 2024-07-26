c889e5dce951f5a0b5e86098cfb681301a4ce41c
kelvintawe12 <k.tawe@alustudent.com>
2024-07-26 02:29:06 +0000

    welcome to multiple tasks

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -19,7 +19,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
-    
+    data = {'email': email}    
     response = requests.post(url, data=data)
     print(response.text)
