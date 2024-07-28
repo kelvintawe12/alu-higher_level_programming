e0e39893f791128385d5e79f74e03c2ebc2dc188
kelvintawe12 <k.tawe@alustudent.com>
2024-07-28 13:45:59 +0000

    What is my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""Docementing and importing the urllib.request"""
+"""nd display the status of a URL"""
 
 
 import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    url = 'https://intranet.hbtn.io/status'
+    
+    # Create a request object with a User-Agent header
+    request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    
+    try:
+        with urllib.request.urlopen(request) as response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print("HTTPError: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print("URLError: {}".format(e.reason))
