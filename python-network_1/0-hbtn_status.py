36adcdb52b4a1956a7aca71f07e9240302298889
kelvintawe12 <k.tawe@alustudent.com>
2024-07-28 15:03:48 +0200

    document module and fix pep8 validation

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,17 +2,19 @@
 """ Fetches and displays the status of a URL."""
 
 import urllib.request
-#imports the requests.
+# imports the requests.
+
+
 def fetch_status():
-#Fetches the content of a URL and prints its type, cont.
+    """ Fetches the content of a URL and prints its type, cont """
     url = 'https://alu-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
-#opens the url.
     print("Body response:")
     print(f"    - type: {type(body)}")
     print(f"    - content: {body}")
     print(f"    - utf8 content: {body.decode('utf-8')}")
-#the print .
+
+
 if __name__ == "__main__":
     fetch_status()
