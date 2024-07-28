90f3bb0b2b3cbab349cb1b832dd8d11d3cf67386
kelvintawe12 <k.tawe@alustudent.com>
2024-07-28 14:18:27 +0000

    what is my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""Displays the URL fetched and its content."""
-
+"""
+fetching and ..
+"""
 
 import urllib.request
-import urllib.error
 
-
-def fetch_url(url):
-    """Fetches the URL and prints its content."""
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    request = urllib.request.Request(url, headers=headers)
-    
-    try:
-        with urllib.request.urlopen(request) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print("HTTPError: {}".format(e.code))
-    except urllib.error.URLError as e:
-        print("URLError: {}".format(e.reason))
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-    # Fetching from 'https://intranet.hbtn.io/status'
-    fetch_url('https://intranet.hbtn.io/status')
-    
-    # Fetching from 'http://0.0.0.0:5050/status'
-    fetch_url('http://0.0.0.0:5050/status')
+    with urllib.request.urlopen(url) as f:
+        content = f.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
