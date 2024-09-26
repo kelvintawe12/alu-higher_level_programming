396b31c166adbb84aa7aa148b5880166a47247f9
kelvintawe12 <k.tawe@alustudent.com>
2024-09-26 21:49:51 +0000

    resctangle to square

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (this._isValid(w) && this._isValid(h)) {
       this.width = w;
       this.height = h;
     } else {
-      // Create an empty object
       return {};
     }
   }
 
-  _isValid(value) {
+  _isValid (value) {
     return Number.isInteger(value) && value > 0;
   }
 }
